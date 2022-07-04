96e7b6ad478d3e6ac7397be6f2811dc8959e2ab8
Anne Monteiro <nanykmc20@gmail.com>
2022-07-04 16:13:00 -0300

    Criação do projeto "Funcionario" no ByteBank (utilizando Herança e Polimorfismo)

---
+++ src/main/java/Aplication/TestFuncionario.java
@@ -0,0 +1,18 @@
+package Aplication;
+
+public class TestFuncionario {
+
+    public static void main(String[] args) {
+
+    Funcionario anne = new Funcionario();
+    anne.setNome("GABRIEL CAETANO");
+    anne.setCpf("123.456.789-01");
+    anne.setSalario(5000.00);
+
+        System.out.println("NOME DO FUNCIONARIO: "+anne.getNome());
+        System.out.println("CPF: " +anne.getCpf());
+        System.out.println("VALOR DA BONIFICAÇÃO: " +anne.getbonificacao());
+
+}
+
+}
